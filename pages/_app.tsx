97b1cc6fcb51328ad5d95d2fba595e217fc51cf2
fayez-baig <coderninja16@gmail.com>
2022-03-21 15:40:35 +0530

    graphQL setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,17 @@
  */
 
 import '../styles/globals.css';
+import { ApolloProvider } from '@apollo/client';
+import client from 'common/client';
 
 import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import { wrapper } from '../store/store';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
+    <ApolloProvider client={client}>
+        <Component {...pageProps} />
+    </ApolloProvider>
+);
 
 export default wrapper.withRedux(MyApp);
